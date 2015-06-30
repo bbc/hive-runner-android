c9310a295c5071c939499dff977f927491589570
asim <asimk21@gmail.com>
2015-06-30 10:28:30 +0100

    Wifi Implementation

--- lib/hive/diagnostic/android/wifi.rb
+++ lib/hive/diagnostic/android/wifi.rb
@@ -3,19 +3,37 @@ module Hive
 	class Diagnostic
 		class Android
 			class Wifi < Diagnostic
-				def check_wifi
-      				begin
-      					status = DeviceAPI::Android::ADB.wifi(@options['serial'])[:status].scan(/^[^\/]*/)[0]
-						wifi = DeviceAPI::Android::ADB.wifi(@options['serial'])[:wifi]
-						message = status == "CONNECTED" ? "#{status} to #{wifi}" : "Disconnected from wifi: #{wifi}" 
-						record_result("wifi", status, message)
-						Hive.logger.info("Wifi: #{wifi} \t Status: #{status}")
-					rescue DiagnosticFailed => e
-							 @log.info("#{e.message}\n");
-					end
-				#record_result(status,message)
-    		  	end
+				attr_accessor :status, :wifi, :message
 
+				def wifi_status
+					@status = DeviceAPI::Android::ADB.wifi(@options['serial'])[:status].scan(/^[^\/]*/)[0]
+					@wifi = DeviceAPI::Android::ADB.wifi(@options['serial'])[:wifi]	
+				end
+
+				def check_wifi
+					begin
+      					wifi_status
+      					if @status.capitalize == @criteria then
+							@message = "#{@status} to #{@wifi}"
+							Hive.logger.info("Wifi: #{@wifi} \t Status: #{@status}") 
+						else
+							Hive.logger.info("Wifi: #{@wifi} \t Status: #{@status}\n Trying Repair")
+							repair_wifi
+							@message = @status == "CONNECTED" ? "#{@status} to #{@wifi}" : "Wi-Fi Disconnected : #{@wifi}"
+						end
+					rescue DiagnosticFailed => e
+							@log.error("Check Wifi Status failed : " + e.message)
+					end
+				 record_result("wifi", @status, @message)
+			  	end
+
+			  	def repair_wifi
+			  		######
+			  		# Wifi repair Logic
+			  		######
+			  			wifi_status
+						return true
+			 	end
 			end
 		end
 	end
