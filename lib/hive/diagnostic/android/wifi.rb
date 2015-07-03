f06ffb50a4bc3d081c483f7577bf571062f6eb8a
asim <asimk21@gmail.com>
2015-07-03 14:41:56 +0100

    Added repair - to toggle ON wifi

--- lib/hive/diagnostic/android/wifi.rb
+++ lib/hive/diagnostic/android/wifi.rb
@@ -3,34 +3,36 @@ module Hive
 	class Diagnostic
 		class Android
 			class Wifi < Diagnostic
-				attr_accessor :status, :wifi, :message
 
-        def diagnose
-          self.fail( :message => 'WIFI on wrong network', {} )
-          self.pass( :message => 'WIFI is fine' )
-        end
-        
-        def repair(result)
-          result.passed!
-        end
+			def device
+				@device = DeviceAPI::Android::ADB
+			end
 
-				def wifi_status
+			def wifi
+				wifi_details = self.device.wifi(@serial)
+				return {:status => wifi_details[:status].scan(/^[^\/]*/)[0], :access_point => wifi_details[:access_point]}
+			end
+
+			def diagnose
+				wifi_status = wifi
+				if wifi_status[:status].capitalize == config[:status] 
+					result = self.fail('Wifi Disconnected', "wifi")
+				else 
+					result = self.pass("Wifi connected to '#{wifi_status[:access_point]}'", "wifi")
 				end
+				result
+			end
 
-				def diagnose
-				  
-				  wifi_status = self.device.wifi
-					
-					result
-					
-					if wifi_status['blah']
-					  result = self.fail( :message => 'Wifi not connected' )
-				  elsif wifi_status['ap'] == config['ap']
-				    result = self.fail( :message => 'Wrong access point' )
-				  end
-				  
-				  result = self.pass(:message => 'Wifi ok') if !result
-				  result
+			def repair(result)
+				Hive.logger.info("Trying to repair wifi")
+				options = {:serial => @serial, :apk => '/opt/resources/wifi-toggle.apk'}#'/Users/khana46/opt/device_api-android/wifi-toggle.apk'}
+				self.device.install_apk(options)
+				self.device.am(@serial, "-n com.wifi.togglewifi/.MainActivity -e wifi true")
+				sleep 5 
+				self.device.uninstall_apk(:serial => @serial, :package_name => 'com.wifi.togglewifi')
+				diagnose
+			end
+	
 			end
 		end
 	end
