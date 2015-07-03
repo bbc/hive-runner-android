e11bbd6852f4ed99e43fe3da40ac41f4ce8dbe76
asim <asimk21@gmail.com>
2015-07-03 15:22:36 +0100

    Added battery diagnostic

--- lib/hive/diagnostic/android/battery.rb
+++ lib/hive/diagnostic/android/battery.rb
@@ -2,20 +2,33 @@ require 'hive/diagnostic'
 module Hive
 	class Diagnostic
 		class Android
-			class Battery < Diagnostic
-				def check_battery
-      		# Should check using device_api-android
-  		    		begin
-      					temperature = DeviceAPI::Android::ADB.get_battery_info(@options['serial'])['temperature']
-      					voltage = DeviceAPI::Android::ADB.get_battery_info(@options['serial'])['voltage']
-      					Hive.logger.info("Battery Temperature: #{temperature} and Voltage: #{voltage} ")
-      				rescue DiagnosticFailed => e
-      		    	  	@log.info("#{e.message}\n");
-      		    	  	#record_result(status,message)
-        			end
+		class Battery < Diagnostic
+
+			def initialize(config, serial)
+				@device = DeviceAPI::Android::ADB
+				super(config, serial)
+			end
+
+			def battery
+			 	self.device.get_battery_info(@serial)
+			end
+			
+			def diagnose
+				battery_details = battery
+				voltage = battery_details['voltage']
+				temperature = battery_details['temperature']
+      			if temperature.to_i < config['temperature'].to_i
+      				result = self.pass("Temperature: #{temperature}\tVoltage:#{voltage}\n Battery status: OK", "battery")
+      			else
+      				result = self.fail("Battery overheated. Temperature: #{temperature}\tVoltage:#{voltage}", "battery")
       			end
+      			result
+      		end
+
+      		def repair(result)
 
 			end
+		end
 		end
 	end
 end
