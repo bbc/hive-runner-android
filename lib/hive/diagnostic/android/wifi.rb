31f6f13c794a948e561c9a3b90fe0b16e68a02f9
asim <asimk21@gmail.com>
2015-07-06 15:50:28 +0100

    Updated diagnose logic

--- lib/hive/diagnostic/android/wifi.rb
+++ lib/hive/diagnostic/android/wifi.rb
@@ -16,25 +16,25 @@ module Hive
 
 			def diagnose
 				wifi_status = wifi
-				if wifi_status[:status].capitalize != config[:status] 
-					result = self.fail('Wifi Disconnected', "wifi")
-				elsif (wifi_status[:access_point] != config[:access_point] || wifi_status[:access_point] != "Xxxxx")
-					result = self.fail("Wifi connected to '#{wifi_status[:access_point]}'", "wifi")
-				else 
-					result = self.pass("Wifi connected to '#{wifi_status[:access_point]}'", "wifi")
+				result = 0
+				config.each do |key, value|
+					if wifi_status[:"#{key}"].capitalize == value.capitalize 
+						result = self.pass("#{key.capitalize} : #{wifi_status[:"#{key}"]}", "wifi" )
+					else
+						result = self.fail(" Error: #{key.capitalize} : #{wifi_status[:"#{key}"]} ", "wifi")
+						break
+					end
 				end
 				result
 			end
 
 			def repair(result)
-				if wifi_status[:status].capitalize != config[:status] 
-					Hive.logger.info("Trying to repair wifi")
-					options = {:serial => @serial, :apk => '/opt/resources/wifi-toggle.apk'}#'/Users/khana46/opt/device_api-android/wifi-toggle.apk'}
-					self.device.install_apk(options)
-					self.device.am(@serial, "-n com.wifi.togglewifi/.MainActivity -e wifi true")
-					sleep 5 
-					self.device.uninstall_apk(:serial => @serial, :package_name => 'com.wifi.togglewifi')
-				end
+				Hive.logger.info("Trying to repair wifi")
+				options = {:serial => @serial, :apk => '/opt/resources/wifi-toggle.apk'}
+				self.device.install_apk(options)
+				self.device.am(@serial, "-n com.wifi.togglewifi/.MainActivity -e wifi true")
+				sleep 5 
+				self.device.uninstall_apk(:serial => @serial, :package_name => 'com.wifi.togglewifi')
 				diagnose
 			end
 	
