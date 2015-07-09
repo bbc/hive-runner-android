9ff2b4ff3fbb93fc0ab2a75ef6680ee8ffbf4bf3
asim <asimk21@gmail.com>
2015-07-09 13:52:18 +0100

    Added diagnose implementation

--- lib/hive/diagnostic/android/memory.rb
+++ lib/hive/diagnostic/android/memory.rb
@@ -4,17 +4,36 @@ module Hive
     class Android
       class Memory < Diagnostic
 
-        def initialize(config, serial)
-          @device = DeviceAPI::Android::ADB
-          super(config, serial)
-        end
-
         def memory
-          true 
+          mem = self.device_api.memory.mem_info
+          return {:free => mem.free.split(' ')[0], 
+                  :total => mem.total.split(' ')[0], 
+                  :used => mem.used.split(' ')[0], }
         end
 
         def diagnose
-          memory_status = memory      
+          result = nil
+          operator = {:free => :>, :used => :< , :total => :==}
+          memory_status = memory
+          begin
+            if config != nil && config.keys.count != 0
+              config.each do |key, value|
+                  if memory_status[:"#{key}"].to_i.send(operator[:"#{key}"], value.to_i) 
+                  result = self.pass("#{key.capitalize} : #{memory_status[:"#{key}"]}", "memory" )
+                  else
+                   result = self.fail("Error: #{key.capitalize} : #{memory_status[:"#{key}"]} ", "memory")
+                   break
+                  end
+               end 
+            else 
+              result = self.pass("No parameter specified", "memory")
+            end
+          
+          rescue
+            Hive.logger.error("Invalid Memory Parameter")
+          raise InvalidParameterError.new("Invalid Memory Parameter") if !result 
+        result  
+        end
         end
 
         def repair(result)
