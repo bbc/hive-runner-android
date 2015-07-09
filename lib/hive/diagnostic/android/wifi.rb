a686653ee3e46251fe9288d8d2311a4b53d01870
asim <asimk21@gmail.com>
2015-07-09 13:51:42 +0100

    Added rescue

--- lib/hive/diagnostic/android/wifi.rb
+++ lib/hive/diagnostic/android/wifi.rb
@@ -2,7 +2,7 @@ require 'hive/diagnostic'
 module Hive
 	class Diagnostic
 		class Android
-			class Wifi < Diagnostic
+		class Wifi < Diagnostic
 
 			def wifi
 				wifi_details = self.device_api.wifi_status
@@ -11,21 +11,30 @@ module Hive
 
 			def diagnose
 				wifi_status = wifi
-				result = 0
+				result = nil
 				config.each do |key, value|
-					if wifi_status[:"#{key}"].capitalize == value.capitalize 
-						result = self.pass("#{key.capitalize} : #{wifi_status[:"#{key}"]}", "wifi" )
-					else
-						result = self.fail(" Error: #{key.capitalize} : #{wifi_status[:"#{key}"]} ", "wifi")
-						break
+				if config != nil && config.keys.count != 0
+					begin
+						if wifi_status[:"#{key}"].capitalize == value.capitalize 
+							result = self.pass("#{key.capitalize} : #{wifi_status[:"#{key}"]}", "wifi" )
+						else
+							result = self.fail(" Error: #{key.capitalize} : #{wifi_status[:"#{key}"]} ", "wifi")
+							break
+						end
+					rescue 
+						Hive.logger.error("Invalid Parameter")
+					raise InvalidParameter.new("Invalid Wifi Parameter: #{key}") if !result
 					end
+				else
+					result = self.pass("No parameter specified", "memory")
+				end
 				end
 				result
-			end
+			end 
 
 			def repair(result)
 				Hive.logger.info("Trying to repair wifi")
-				options = {:serial => @serial, :apk => '/opt/resources/wifi-toggle.apk'}
+				options = {:serial => @serial, :apk => '/opt/resources/wifi-toggle.apk'} 
 				self.device.install_apk(options)
 				self.device.am(@serial, "-n com.wifi.togglewifi/.MainActivity -e wifi true")
 				sleep 5 
@@ -33,7 +42,7 @@ module Hive
 				diagnose
 			end
 	
-			end
+		end
 		end
 	end
 end
