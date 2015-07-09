35f9c6dda0e1a7429db6bc22f0c8070aaf57bbd5
asim <asimk21@gmail.com>
2015-07-09 14:18:05 +0100

    modified operator

--- lib/hive/diagnostic/android/memory.rb
+++ lib/hive/diagnostic/android/memory.rb
@@ -13,27 +13,26 @@ module Hive
 
         def diagnose
           result = nil
-          operator = {:free => :>, :used => :< , :total => :==}
+          operator = {:free => :>=, :used => :<= , :total => :==}
           memory_status = memory
           begin
             if config != nil && config.keys.count != 0
               config.each do |key, value|
                   if memory_status[:"#{key}"].to_i.send(operator[:"#{key}"], value.to_i) 
-                  result = self.pass("#{key.capitalize} : #{memory_status[:"#{key}"]}", "memory" )
+                  result = self.pass("#{key.capitalize} Memory (#{memory_status[:"#{key}"]}) #{operator[:"#{key}"]} #{value}", "memory" )
                   else
-                   result = self.fail("Error: #{key.capitalize} : #{memory_status[:"#{key}"]} ", "memory")
-                   break
+                    result = self.fail("Error: #{key.capitalize} Memory (#{memory_status[:"#{key}"]}) is not #{operator[:"#{key}"]} #{value}", "memory")
+                    break
                   end
-               end 
+              end 
             else 
               result = self.pass("No parameter specified", "memory")
             end
-          
           rescue
             Hive.logger.error("Invalid Memory Parameter")
-          raise InvalidParameterError.new("Invalid Memory Parameter") if !result 
-        result  
+          raise InvalidParameterError.new("Invalid Memory Parameter") if !result   
         end
+        result
         end
 
         def repair(result)
