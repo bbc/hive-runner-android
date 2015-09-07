ec334eee3543f4244a5b29722120085f6221824a
asim <asimk21@gmail.com>
2015-09-07 16:54:31 +0100

    Fixed Indentation

--- lib/hive/diagnostic/android/battery.rb
+++ lib/hive/diagnostic/android/battery.rb
@@ -1,38 +1,38 @@
 require 'hive/diagnostic'
 module Hive
-	class Diagnostic
-		class Android
-		class Battery < Diagnostic
+  class Diagnostic
+    class Android
+    class Battery < Diagnostic
 
-			def battery
-				self.device_api.battery_info
-			end
-			
-			def diagnose
-			result = nil
-			battery_info = battery
-			begin
-				if config != nil && config.keys.count != 0	
-					temperature = battery_info['temperature']
-					if temperature.to_i < config['temperature'].to_i
-						result = self.pass("Temperature: #{temperature}\n Battery status: OK", "battery")
-					else
-						result = self.fail("Battery overheated. Temperature: #{temperature} ", "battery")
-					end
-				else
-					result = self.pass("No parameter specified for battery", "battery")
-				end
-			rescue
-            	Hive.logger.error("Invalid Battery Parameter")
-          		raise InvalidParameterError.new("Invalid Parameter for battery") if !result
-          	end
-			result
-			end
+      def battery
+        self.device_api.battery_info
+      end
+      
+      def diagnose
+      result = nil
+      battery_info = battery
+      begin
+        if config != nil && config.keys.count != 0  
+          temperature = battery_info['temperature']
+          if temperature.to_i < config['temperature'].to_i
+            result = self.pass("Temperature: #{temperature}\n Battery status: OK", "battery")
+          else
+            result = self.fail("Battery overheated. Temperature: #{temperature} ", "battery")
+          end
+        else
+          result = self.pass("No parameter specified for battery", "battery")
+        end
+      rescue
+              Hive.logger.error("Invalid Battery Parameter")
+              raise InvalidParameterError.new("Invalid Parameter for battery") if !result
+            end
+      result
+      end
 
-			def repair(result)
-				diagnose
-			end
-		end
-		end
-	end
+      def repair(result)
+        diagnose
+      end
+    end
+    end
+  end
 end
--- lib/hive/diagnostic/android/wifi.rb
+++ lib/hive/diagnostic/android/wifi.rb
@@ -1,48 +1,48 @@
 require 'hive/diagnostic'
 module Hive
-	class Diagnostic
-		class Android
-		class Wifi < Diagnostic
+  class Diagnostic
+    class Android
+    class Wifi < Diagnostic
 
-			def wifi
-				wifi_details = self.device_api.wifi_status
-				return {:status => wifi_details[:status].scan(/^[^\/]*/)[0], :access_point => wifi_details[:access_point]}
-			end
+      def wifi
+        wifi_details = self.device_api.wifi_status
+        return {:status => wifi_details[:status].scan(/^[^\/]*/)[0], :access_point => wifi_details[:access_point]}
+      end
 
-			def diagnose
-			result = nil
-			wifi_status = wifi
-			config.each do |key, value|
-				if config != nil && config.keys.count != 0
-					begin
-						if wifi_status[:"#{key}"].capitalize == value.capitalize 
-							result = self.pass("#{key.capitalize} : #{wifi_status[:"#{key}"]}", "wifi" )
-						else
-							result = self.fail(" Error: #{key.capitalize} : #{wifi_status[:"#{key}"]} ", "wifi")
-							break
-						end
-					rescue 
-						Hive.logger.error("Invalid Parameter for Wifi #{key}")
-						raise InvalidParameter.new("Invalid Wifi Parameter for Wifi: #{key}") if !result
-					end
-				else
-					result = self.pass("No parameter specified", "wifi")
-				end
-			end
-			result
-			end 
+      def diagnose
+      result = nil
+      wifi_status = wifi
+      config.each do |key, value|
+        if config != nil && config.keys.count != 0
+          begin
+            if wifi_status[:"#{key}"].capitalize == value.capitalize 
+              result = self.pass("#{key.capitalize} : #{wifi_status[:"#{key}"]}", "wifi" )
+            else
+              result = self.fail(" Error: #{key.capitalize} : #{wifi_status[:"#{key}"]} ", "wifi")
+              break
+            end
+          rescue 
+            Hive.logger.error("Invalid Parameter for Wifi #{key}")
+            raise InvalidParameter.new("Invalid Wifi Parameter for Wifi: #{key}") if !result
+          end
+        else
+          result = self.pass("No parameter specified", "wifi")
+        end
+      end
+      result
+      end 
 
-			def repair(result)
-				Hive.logger.info("Trying to repair wifi")
-				options = {:apk => '/path/to/apk/to/toggle/wifi', :package => '/pkg/name/ex: com.wifi.togglewifi'} 
-				self.device_api.install(options[:apk])
-				self.device_api.start_intent("-n com.wifi.togglewifi/.MainActivity -e wifi true")
-				sleep 5 
-				self.device_api.uninstall(options[:package])
-				diagnose
-			end
-	
-		end
-		end
-	end
+      def repair(result)
+        Hive.logger.info("Trying to repair wifi")
+        options = {:apk => '/path/to/apk/to/toggle/wifi', :package => '/pkg/name/ex: com.wifi.togglewifi'} 
+        self.device_api.install(options[:apk])
+        self.device_api.start_intent("-n com.wifi.togglewifi/.MainActivity -e wifi true")
+        sleep 5 
+        self.device_api.uninstall(options[:package])
+        diagnose
+      end
+  
+    end
+    end
+  end
 end
