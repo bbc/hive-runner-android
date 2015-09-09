9e635e71110ff5a0f1e0957a3dbe7183ae7c1a5c
asim <asimk21@gmail.com>
2015-09-09 15:43:37 +0100

    Added diagnostic for rebooting

---
+++ lib/hive/diagnostic/android/uptime.rb
@@ -0,0 +1,41 @@
+require 'hive/diagnostic'
+module Hive
+  class Diagnostic
+    class Android
+      class Uptime < Diagnostic
+        
+        def initialize(config, options)
+          @next_reboot_time = Time.now + config[:reboot_timeout] if config.has_key?(:reboot_timeout)
+          super(config, options)
+        end
+
+        def diagnose
+          require 'pry'
+          binding.pry
+          if config.has_key?(:reboot_timeout)
+            if Time.now < @next_reboot_time
+              self.pass("Time to next reboot: #{@next_reboot_time - Time.now}s")
+            else
+              self.fail("Reboot required")
+            end
+          else
+            self.pass("Not configured to reboot")
+          end
+        end
+        
+        def repair(result)
+          Hive.logger.info("Rebooting the device")
+          begin  
+            self.device_api.reboot
+            sleep 30
+          rescue
+            Hive.logger.error("Device not found")
+          end
+          @next_reboot_time += config[:reboot_timeout]
+          self.pass("Time to next reboot: #{@next_reboot_time - Time.now}s")
+        end
+
+      end
+    end
+  end
+end
