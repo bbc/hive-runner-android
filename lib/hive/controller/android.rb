4cc0cffae9f6a9f5489a303d0a494b3e6724b7ad
Jon Wilson <jon.wilson01@bbc.co.uk>
2016-04-12 10:43:39 +0100

    Removed 'detect_hive_mind' as there's now only one detect method. Removed 'HM) ' from debug messages

--- lib/hive/controller/android.rb
+++ lib/hive/controller/android.rb
@@ -6,14 +6,10 @@ module Hive
   class Controller
     class Android < Controller
 
+      # Register and poll connected devices
       def detect
-        self.detect_hive_mind
-      end
-
-      # Register with Hive Mind (New)
-      def detect_hive_mind
         devices = DeviceAPI::Android.devices
-        Hive.logger.debug('HM) No devices attached') if devices.empty?
+        Hive.logger.debug('No devices attached') if devices.empty?
 
         if not Hive.hive_mind.device_details.has_key? :error
           # Selecting only android mobiles
@@ -22,22 +18,22 @@ module Hive
           to_poll = []
           attached_devices = []
           connected_devices.each do |device|
-            Hive.logger.debug("HM) Device details: #{device.inspect}")
+            Hive.logger.debug("Device details: #{device.inspect}")
             registered_device = devices.select { |a| a.serial == device['serial'] && a.status != :unauthorized && a.status != :no_permissions}
             if registered_device.empty?
               # A previously registered device isn't attached
-              Hive.logger.debug("HM) A previously registered device has disappeared: #{device}")
+              Hive.logger.debug("A previously registered device has disappeared: #{device}")
             else
               # A previously registered device is attached, poll it
-              Hive.logger.debug("HM) Setting #{device} to be polled")
-              Hive.logger.info("HM) Device: #{registered_device.inspect}")
+              Hive.logger.debug("Setting #{device} to be polled")
+              Hive.logger.debug("Device: #{registered_device.inspect}")
               begin
                 attached_devices << self.create_device(device.merge('os_version' => registered_device[0].version))
                 to_poll << device['id']
               rescue DeviceAPI::DeviceNotFound => e
-                Hive.logger.warn("HM) Device disconnected before registration (serial: #{device['serial']})")
+                Hive.logger.warn("Device disconnected before registration (serial: #{device['serial']})")
               rescue => e
-                Hive.logger.warn("HM) Error with connected device: #{e.message}")
+                Hive.logger.warn("Error with connected device: #{e.message}")
               end
 
               devices = devices - registered_device
@@ -45,7 +41,7 @@ module Hive
           end
 
           # Poll already registered devices
-          Hive.logger.debug("HM) Polling: #{to_poll}")
+          Hive.logger.debug("Polling: #{to_poll}")
           Hive.hive_mind.poll(*to_poll)
 
           # Register new devices
@@ -64,17 +60,17 @@ module Hive
                   operating_system_version: device.version
               )
               Hive.hive_mind.connect(dev['id'])
-              Hive.logger.info("HM) Device registered: #{dev}")
+              Hive.logger.info("Device registered: #{dev}")
             rescue DeviceAPI::DeviceNotFound => e
-              Hive.logger.warn("HM) Device disconnected before registration (serial: #{device.serial})")
+              Hive.logger.warn("Device disconnected before registration (serial: #{device.serial})")
             rescue => e
-              Hive.logger.warn("HM) Error with connected device: #{e.message}")
+              Hive.logger.warn("Error with connected device: #{e.message}")
             end
           end
 
         else
-          Hive.logger.info('HM) No Hive Mind connection')
-          Hive.logger.debug("HM) Error: #{Hive.hive_mind.device_details[:error]}")
+          Hive.logger.info('No Hive Mind connection')
+          Hive.logger.debug("Error: #{Hive.hive_mind.device_details[:error]}")
           # Hive Mind isn't available, use DeviceAPI instead
           device_info = devices.select { |a| a.status != :unauthorized && a.status != :no_permissions }.map do |device|
             {
