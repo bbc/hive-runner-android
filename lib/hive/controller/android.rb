db0e00e2a9480dc4fd73ceab15158eee41652703
David Craddock <contact@davidcraddock.net>
2016-06-29 13:17:34 +0100

    Refactored controller

--- lib/hive/controller/android.rb
+++ lib/hive/controller/android.rb
@@ -5,81 +5,42 @@ require 'device_api/android'
 module Hive
   class Controller
     class Android < Controller
+      attr_accessor :devices
 
-      # Register and poll connected devices
       def detect
-        devices = DeviceAPI::Android.devices
+        all_attached_devices = get_connected_devices # get all connected devices
+        to_poll = select_devices(all_attached_devices) # select devices to poll
+        poll_devices(to_poll) # poll devices
+        register_new_devices(devices) # register new devices with Hivemind
+        all_attached_devices
+      end
+
+      def get_connected_devices
+        # get a list of connected devices from Hivemind or DeviceAPI
+        @devices = DeviceAPI::Android.devices
         Hive.logger.debug('No devices attached') if devices.empty?
 
-        if not Hive.hive_mind.device_details.has_key? :error
+        unless Hive.hive_mind.device_details.has_key?(:error)
           # Selecting only android mobiles
-          connected_devices = Hive.hive_mind.device_details['connected_devices'].select{ |d| d['device_type'] == 'Mobile' && d['operating_system_name'] == 'android' }
-
-          to_poll = []
-          attached_devices = []
-          connected_devices.each do |device|
-            Hive.logger.debug("Device details: #{device.inspect}")
-            registered_device = devices.select { |a| a.serial == device['serial'] && a.status != :unauthorized && a.status != :no_permissions}
-            if registered_device.empty?
-              # A previously registered device isn't attached
-              Hive.logger.debug("A previously registered device has disappeared: #{device}")
-            else
-              # A previously registered device is attached, poll it
-              Hive.logger.debug("Setting #{device} to be polled")
-              Hive.logger.debug("Device: #{registered_device.inspect}")
-              begin
-                attached_devices << self.create_device(device.merge('os_version' => registered_device[0].version))
-                to_poll << device['id']
-              rescue DeviceAPI::DeviceNotFound => e
-                Hive.logger.warn("Device disconnected before registration (serial: #{device['serial']})")
-              rescue => e
-                Hive.logger.warn("Error with connected device: #{e.message}")
-              end
-
-              devices = devices - registered_device
-            end
+          connected_devices = Hive.hive_mind.device_details['connected_devices'].select do |d|
+                d['device_type'] == 'Mobile' &&
+                d['operating_system_name'] == 'android'
           end
-
-          # Poll already registered devices
-          Hive.logger.debug("Polling: #{to_poll}")
-          Hive.hive_mind.poll(*to_poll)
-
-          # Register new devices
-          devices.select{|a| a.status != :unauthorized && a.status != :no_permissions}.each do |device|
-            begin
-              dev = Hive.hive_mind.register(
-                  hostname: device.model,
-                  serial: device.serial,
-                  macs: [device.wifi_mac_address],
-                  ips: [device.ip_address],
-                  brand: device.manufacturer.capitalize,
-                  model: device.model,
-                  device_type: 'Mobile',
-                  imei: device.imei,
-                  operating_system_name: 'android',
-                  operating_system_version: device.version
-              )
-              Hive.hive_mind.connect(dev['id'])
-              Hive.logger.info("Device registered: #{dev}")
-            rescue DeviceAPI::DeviceNotFound => e
-              Hive.logger.warn("Device disconnected before registration (serial: #{device.serial})")
-            rescue => e
-              Hive.logger.warn("Error with connected device: #{e.message}")
-            end
-          end
-
         else
           Hive.logger.info('No Hive Mind connection')
           Hive.logger.debug("Error: #{Hive.hive_mind.device_details[:error]}")
           # Hive Mind isn't available, use DeviceAPI instead
-          device_info = devices.select { |a| a.status != :unauthorized && a.status != :no_permissions }.map do |device|
+          device_info = @devices.select do |a|
+              a.status != :unauthorized &&
+              a.status != :no_permissions
+            end.map do |device|
             {
-              'id' =>  device.serial,
-              'serial' => device.serial,
-              'status' => 'idle',
-              'model' => device.model,
-              'brand' => device.manufacturer,
-              'os_version' => device.version
+             'id' => device.serial,
+             'serial' => device.serial,
+             'status' => 'idle',
+             'model' => device.model,
+             'brand' => device.manufacturer,
+             'os_version' => device.version
             }
           end
 
@@ -89,7 +50,76 @@ module Hive
         end
 
         Hive.logger.info(attached_devices)
-        attached_devices
+        connected_devices
+      end
+
+      def select_devices(connected_devices)
+        # select devices that we want to poll
+        attached_devices = []
+        to_poll = []
+        connected_devices.each do |device|
+          Hive.logger.debug("Device details: #{device.inspect}")
+          registered_device = @devices.select do |a|
+            a.serial == device['serial'] &&
+                a.status != :unauthorized &&
+                a.status != :no_permissions
+          end
+
+          if registered_device.empty? # A previously registered device isn't attached
+            Hive.logger.debug("A previously registered device has disappeared: #{device}")
+          else # A previously registered device is attached, poll it
+            Hive.logger.debug("Setting #{device} to be polled")
+            Hive.logger.debug("Device: #{registered_device.inspect}")
+
+            begin
+              attached_devices << self.create_device(device.merge('os_version' => registered_device[0].version))
+              to_poll << device['id']
+
+            rescue DeviceAPI::DeviceNotFound => e
+              Hive.logger.warn("Device disconnected before registration (serial: #{device['serial']})")
+            rescue => e
+              Hive.logger.warn("Error with connected device: #{e.message}")
+            end
+
+            @devices = @devices - registered_device
+          end
+        end
+
+        to_poll
+      end
+
+
+      def poll_devices(to_poll)
+        # poll selected devices
+
+        Hive.logger.debug("Polling: #{to_poll}")
+        Hive.hive_mind.poll(*to_poll)
+      end
+
+      def register_new_devices(devices)
+        # Register new devices with Hivemind
+        @devices.select { |a| a.status != :unauthorized && a.status != :no_permissions }.each do |device|
+          begin
+            dev = Hive.hive_mind.register(
+                hostname: device.model,
+                serial: device.serial,
+                macs: [device.wifi_mac_address],
+                ips: [device.ip_address],
+                brand: device.manufacturer.capitalize,
+                model: device.model,
+                device_type: 'Mobile',
+                imei: device.imei,
+                operating_system_name: 'android',
+                operating_system_version: device.version
+            )
+            Hive.hive_mind.connect(dev['id'])
+            Hive.logger.info("Device registered: #{dev}")
+          rescue DeviceAPI::DeviceNotFound => e
+            Hive.logger.warn("Device disconnected before registration (serial: #{device.serial})")
+          rescue => e
+            Hive.logger.warn("Error with connected device: #{e.message}")
+          end
+        end
       end
 
       def display_devices(hive_details)
@@ -100,7 +130,7 @@ module Hive
               [
                   "#{device['device_brand']} #{device['device_model']}",
                   device['serial'],
-                  (device['device_queues'].map { |queue| queue['name']}).join("\n"),
+                  (device['device_queues'].map { |queue| queue['name'] }).join("\n"),
                   device['status']
               ]
             end
@@ -110,6 +140,13 @@ module Hive
 
         Hive.logger.info(table)
       end
+    end
+
+  end
+
+  class DeviceError < StandardError
+    def initialize(msg)
+      super(msg)
     end
   end
 end
