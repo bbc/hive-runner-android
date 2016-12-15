9af4f453046c2cadb4e35c1910e96490e7ae2b31
asim <asimk21@gmail.com>
2016-12-15 13:05:59 +0000

    Bug fix to report correct memory information instead of config value

--- lib/hive/diagnostic/android/memory.rb
+++ lib/hive/diagnostic/android/memory.rb
@@ -19,7 +19,7 @@ module Hive
           memory_status = memory
           config.each do |key, value|
             raise InvalidParameterError.new("Battery Parameter should be any of ':free', ':used', ':total'") if !memory_status.has_key? key.to_sym
-            data[:"#{key}_memory"] = {:value => value, :unit => "kB"}
+            data[:"#{key}_memory"] = {:value => memory_status[:"#{key}"], :unit => "kB"}
             result = "fail" if !memory_status[:"#{key}"].to_i.send(operator[:"#{key}"], value.to_i)
           end 
 
