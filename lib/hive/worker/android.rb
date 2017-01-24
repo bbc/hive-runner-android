004e573c4790f6614f3f6a87f738fc8ca310c45c
Joe Haig <josephhaig@gmail.com>
2017-01-24 09:39:21 +0000

    Only sign apk if resign is true

--- lib/hive/worker/android.rb
+++ lib/hive/worker/android.rb
@@ -66,8 +66,11 @@ module Hive
         script.set_env "APK_PATH", apk_path
         if job.build
           @log.debug("Re-signing APK: #{job.resign}")
-          file_system.fetch_build(job.build, apk_path)
-          DeviceAPI::Android::Signing.sign_apk({apk: apk_path, resign: job.resign})
+          if job.resign
+            file_system.fetch_build(job.build, apk_path)
+            DeviceAPI::Android::Signing.sign_apk({apk: apk_path, resign: true})
+          end
+          @log.debug("Signing done")
         end
 
         DeviceAPI::Android.device(device['serial']).unlock
